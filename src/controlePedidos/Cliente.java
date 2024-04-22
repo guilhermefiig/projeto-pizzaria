562bdcaf24c75efb87f3983fb21865fa8b8a25e5
Guilherme Figueirêdo <guilherme.figueiredosc@gmail.com>
2024-04-22 18:29:26 -0300

    Construtores, getters e setters

--- src/controlePedidos/Cliente.java
+++ src/controlePedidos/Cliente.java
@@ -1,4 +1,17 @@
 package controlePedidos;
 
 public class Cliente {
+    private String nome;
+
+    public Cliente(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 }
